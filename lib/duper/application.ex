f80849f6a88e8b6dc9e63e155e0ff6e8b0544284
Your Name <kevin.kds80@gmail.com>
2021-12-01 23:13:51 +0000

    add modules to application config

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -12,7 +12,8 @@ defmodule Duper.Application do
       # {Duper.Worker, arg}
       Duper.Results,
       { Duper.PathFinder, "." },
-      Duper.WorkerSupervisor
+      Duper.WorkerSupervisor,
+      { Duper.Gatherer, 1 }
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
